4a26da031e60277f327249cb955edb61ed871328
Xavin616 <jacexavinbello@gmail.com>
2023-08-04 14:25:17 +0100

    #5 Tabs in sidebar

--- components/custom/sidebar.jsx
+++ components/custom/sidebar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Bell, HelpCircle, Settings } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 
 function SidebarItem({ icon, link, name }) {
   return (
@@ -18,7 +19,7 @@ function SidebarItem({ icon, link, name }) {
 
 function EditorSidebar() {
   return (
-    <div className="w-full h-full p-3 flex flex-col justify-between items-center flex-nowrap overflow-hidden">
+    <div className="w-full h-full p-3 flex flex-col justify-start items-center flex-nowrap space-y-6 overflow-hidden">
       <div className="w-full mb-4 px-3 flex justify-start items-center space-x-3">
         <Avatar>
           <AvatarImage src="https://pbs.twimg.com/profile_images/1509994781476376586/s_iiM2YN_400x400.jpg" />
@@ -29,7 +30,14 @@ function EditorSidebar() {
           <span className="font-semibold text-sm">JIO</span>
         </div>
       </div>
-      <div className="flex flex-col justify-start items-start">Hello</div>
+      <Tabs defaultValue="drafts" className="w-64 h-[60%]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="drafts">Drafts</TabsTrigger>
+          <TabsTrigger value="posted">Posted</TabsTrigger>
+        </TabsList>
+        <TabsContent value="drafts">Drafts 1</TabsContent>
+        <TabsContent value="posted">Posted 1</TabsContent>
+      </Tabs>
       <div className="w-full grid items-start gap-1">
         <SidebarItem
           icon={<Bell className="h-5 w-5 group-hover:stroke-rose-500" />}
